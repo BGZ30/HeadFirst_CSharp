620a2867d714e36596b8d089cac375e4bd8de227
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-03 12:58:53 +0200

    Create three interactive buttons

---
+++ Basic_Components/Buttons/buttonsThree/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace buttonsThree
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+
+            EventHandler handler = new EventHandler(button_Click);
+
+            red = new Button();  
+            red.Text = "Red";  
+            red.Location = new Point(100, 50);  
+            red.Size = new Size(50, 50);  
+            red.Click +=handler;  
+            Controls.Add(red);  
+        
+            blue = new Button();  
+            blue.Text = "Blue";  
+            blue.Location = new Point(100, 100);  
+            blue.Size = new Size(50, 50);  
+            blue.Click += handler;  
+            Controls.Add(blue);  
+        
+            green = new Button();  
+            green.Text = "Green";  
+            green.Location = new Point(100, 150);  
+            green.Size = new Size(50, 50);  
+            green.Click += handler;  
+            Controls.Add(green);
+        }
+
+        #endregion
+    }
+}
+
---
+++ Basic_Components/Buttons/buttonsThree/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buttonsThree
+{
+    public partial class Form1 : Form
+    {
+        private Button red;  
+        private Button blue;  
+        private Button green; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e) {  
+            if (sender == red) 
+                this.BackColor = Color.Red ;  
+            else if (sender == blue) 
+                this.BackColor = Color.Blue;  
+            else 
+                this.BackColor = Color.Green;  
+        }
+
+    }
+}
